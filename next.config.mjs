597c372e786c03050a5b023141bb919ede550af0
ShahVandit8 <shahvandit8@gmail.com>
2025-04-11 22:01:08 -0400

    fix(config): fixed next config file for redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'vandit-shah.me' }],
-        destination: 'https://vandit-shah.me/:path*/',
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
